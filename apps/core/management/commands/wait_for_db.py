1d71c32f4aeddffb73be385fb255862484dc03c1
git-shuvam <github.shuvam@gmail.com>
2021-07-15 14:02:07 +0530

    wait_for_db command issue fixed

--- apps/core/management/commands/wait_for_db.py
+++ apps/core/management/commands/wait_for_db.py
@@ -9,13 +9,13 @@ class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
-        self.stdout.write('waiting for Database...')
-        db_con = None
-        while not db_con:
+        self.stdout.write('Waiting for Database...')
+        db_conn = None
+        while not db_conn:
             try:
-                db_con = connections['default']
+                db_conn = connections['default']
             except OperationalError:
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database available'))
+        self.stdout.write(self.style.SUCCESS('Database available!'))
